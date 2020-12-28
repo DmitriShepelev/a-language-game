29274b753d755ea7d257b931046579c84faaf9a6
Дмитрий Шепелев <shepelev.dm@gmail.com>
2020-12-28 20:06:13 +0300

    Done

--- LanguageGame/Translator.cs
+++ LanguageGame/Translator.cs
@@ -1,4 +1,7 @@
-using System;
+﻿using System;
+using System.Text;
+
+#pragma warning disable S1854
 
 namespace LanguageGame
 {
@@ -20,11 +23,136 @@ namespace LanguageGame
         /// "Eat" -> "Eatyay"
         /// "explain" -> "explainyay"
         /// "Smile" -> "Ilesmay"
-        /// "Glove" -> "Oveglay"
+        /// "Glove" -> "Oveglay".
         /// </example>
         public static string TranslateToPigLatin(string phrase)
         {
-            throw new NotImplementedException("You need to implement this method.");
+            if (string.IsNullOrWhiteSpace(phrase))
+            {
+                throw new ArgumentException("Source string cannot be null or empty or whitespace.");
+            }
+
+            var vowels = new char[] { 'a', 'o', 'i', 'u', 'e' };
+            var consonants = GetConsonants(vowels);
+            var punctuation = new char[] { ' ', '-', ',', '.', '!', '?' };
+            var yay = new char[] { 'y', 'a', 'y' };
+            var ay = new char[] { 'a', 'y' };
+            var result = new StringBuilder(phrase.Length * 2);
+            var generalIndex = 0;
+            while (generalIndex < phrase.Length)
+            {
+                if (IsVowel(phrase[generalIndex], vowels))
+                {
+                    var word = GetWord(generalIndex, phrase, punctuation);
+                    result.Append(word);
+                    result.Append(yay);
+                    generalIndex += word.Length;
+                }
+                else if (IsConsonant(phrase[generalIndex], consonants))
+                {
+                    var word = GetWord(generalIndex, phrase, punctuation);
+                    Transform(word, consonants);
+                    result.Append(word);
+                    result.Append(ay);
+                    generalIndex += word.Length;
+                }
+                else
+                {
+                    result.Append(phrase[generalIndex]);
+                    generalIndex++;
+                }
+            }
+
+            return result.ToString();
+        }
+
+        private static char[] GetWord(int idx, string source, char[] punctuation)
+        {
+            int wordLength;
+            if (source.IndexOfAny(punctuation, idx) > -1)
+            {
+                wordLength = source.IndexOfAny(punctuation, idx);
+            }
+            else
+            {
+                wordLength = source.Length;
+            }
+
+            return source[idx..wordLength].ToCharArray();
+        }
+
+        private static void Transform(char[] word, char[] consonants)
+        {
+            bool upperFlag = char.IsUpper(word[0]);
+            word[0] = char.ToLower(word[0], System.Globalization.CultureInfo.InvariantCulture);
+            for (int j = 0; j < word.Length; j++)
+            {
+                if (IsConsonant(word[0], consonants))
+                {
+                    var tmp = word[0];
+                    for (int i = 0; i < word.Length - 1; i++)
+                    {
+                        word[i] = word[i + 1];
+                    }
+
+                    word[^1] = tmp;
+                }
+            }
+
+            if (upperFlag)
+            {
+                word[0] = char.ToUpper(word[0], System.Globalization.CultureInfo.InvariantCulture);
+            }
+        }
+
+        private static char[] GetConsonants(char[] vowels)
+        {
+            var letters = new char['z' - 'a' + 1];
+            for (int i = 0; i < letters.Length; i++)
+            {
+                letters[i] = (char)((int)'a' + i);
+            }
+
+            var consonants = new char[letters.Length - 5];
+            var j = 0;
+            for (int i = 0; i < letters.Length; i++)
+            {
+                if (!IsVowel(letters[i], vowels))
+                {
+                    consonants[j] = letters[i];
+                    j++;
+                }
+            }
+
+            return consonants;
+        }
+
+        private static bool IsVowel(char ch, char[] vowels)
+        {
+            var culture = System.Globalization.CultureInfo.InvariantCulture;
+            for (int i = 0; i < vowels.Length; i++)
+            {
+                if (char.ToLower(ch, culture) == vowels[i])
+                {
+                    return true;
+                }
+            }
+
+            return false;
+        }
+
+        private static bool IsConsonant(char ch, char[] consonants)
+        {
+            var culture = System.Globalization.CultureInfo.InvariantCulture;
+            for (int i = 0; i < consonants.Length; i++)
+            {
+                if (char.ToLower(ch, culture) == consonants[i])
+                {
+                    return true;
+                }
+            }
+
+            return false;
         }
     }
 }
